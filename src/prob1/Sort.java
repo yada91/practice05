1511d1d111dba4f66f0db48732622f4e22489eab
bit-user <bit-user@bit>
2016-09-21 16:15:29 +0900

    last

--- src/prob1/Sort.java
+++ src/prob1/Sort.java
@@ -5,7 +5,7 @@ public class Sort {
 		int array[] = { 5, 9, 3, 8, 60, 20, 1 };
 		int temp = 0;
 		int count = array.length;
-		int count1 = 0;
+
 		System.out.println("Before sort.");
 
 		for (int i = 0; i < count; i++) {
@@ -15,15 +15,12 @@ public class Sort {
 		//
 		// 선택 정렬 알고리즘이 적용된 코드를 여기에 작성합니다.
 		//
-		while (count1 <= array.length) {
+		for (int j = 0; j < array.length - 1; j++) {
 			for (int i = 0; i < array.length - 1; i++) {
-				if (array[i] >= array[i + 1]) {
-					count1++;
-				} else {
+				if (array[i] < array[i + 1]) {
 					temp = array[i + 1];
 					array[i + 1] = array[i];
 					array[i] = temp;
-					count1 = 0;
 				}
 			}
 		}
